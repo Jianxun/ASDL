297df326ccd316311c43f35f583ae9d7b107eb4c
Jianxun Zhu <zhujianxun.bupt@gmail.com>
2025-09-06 22:53:48 -0400

    cleaned up example folder

---
+++ examples/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PROJECT_ROOT=$(pwd)
+
+# PDK
+export PDK=gf180mcu
+export PDK_PATH=$PROJECT_ROOT/pdks/$PDK
+export PDK_ASDL_PATH=$PDK_PATH/asdl
+export PDK_MODEL_PATH=$PDK_PATH/ngspice
+
+# ASDL common libs
+export ASDL_COMMON_LIBS_PATH=$PROJECT_ROOT/libs_common
+
+# ASDL path
+export ASDL_PATH=$PDK_ASDL_PATH:$ASDL_COMMON_LIBS_PATH
+
+
