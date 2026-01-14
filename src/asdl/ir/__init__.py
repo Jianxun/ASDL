a708073b90ee98fae24898326248ea5ae29e6f52
Jianxun Zhu <zhujianxun.bupt@gmail.com>
2026-01-13 19:54:18 -0500

    Add GraphIR program/module/device ops

--- src/asdl/ir/__init__.py
+++ src/asdl/ir/__init__.py
@@ -2,6 +2,12 @@
 
 from .converters.ast_to_nfir import convert_document
 from .converters.nfir_to_ifir import convert_design as convert_nfir_to_ifir
+from .graphir import ASDL_GRAPHIR
 from .pipeline import run_mvp_pipeline
 
-__all__ = ["convert_document", "convert_nfir_to_ifir", "run_mvp_pipeline"]
+__all__ = [
+    "ASDL_GRAPHIR",
+    "convert_document",
+    "convert_nfir_to_ifir",
+    "run_mvp_pipeline",
+]
---
+++ src/asdl/ir/graphir/__init__.py
@@ -0,0 +1,11 @@
+"""GraphIR dialect package for ASDL."""
+
+from .dialect import ASDL_GRAPHIR, DeviceOp, GraphIdAttr, ModuleOp, ProgramOp
+
+__all__ = [
+    "ASDL_GRAPHIR",
+    "DeviceOp",
+    "GraphIdAttr",
+    "ModuleOp",
+    "ProgramOp",
+]
